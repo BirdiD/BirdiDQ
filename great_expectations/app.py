0cb67775102e715840ce4f3082fcef5ab0cc19fa
BirdiD <diioula.doucoure@gmail.com>
2023-06-19 14:18:34 +0200

    clean code

--- great_expectations/app.py
+++ great_expectations/app.py
@@ -1,8 +1,55 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import plotly.graph_objects as go
+from streamlit_extras.let_it_rain import rain
 import pandas as pd
 import time
 import webbrowser
-from geutils import DataQuality
+from great_expectations.geutils import DataQuality
+
+
+def display_test_result(result):
+    """
+    Display GE json expectation output
+    """
+    # Check if the test was successful
+    success = result["success"]
+
+    # Display the test result box
+    if success:
+        st.success('Your data quality test succeeded!', icon="✅")
+        rain(emoji="🎈",
+             font_size=54,
+             falling_speed=5,
+             animation_length="infinite",
+            )
+    else:
+        st.error('Test failed. View data docs for more details', icon="🚨")
+
+    # Create data for the bar chart
+    labels = ['Element Count', 'Unexpected Count', 'Unexpected Percent']
+    values = [result['result']['element_count'], result['result']['unexpected_count'], result['result']['unexpected_percent']]
+    colors = ['lightskyblue', 'lightcoral', 'lightgreen']
+
+    # Create a bar chart using Plotly
+    fig = go.Figure(data=go.Bar(x=labels, y=values, marker=dict(color=colors)))
+    fig.update_layout(title='Data Quality Test Results', xaxis_title='Metrics', yaxis_title='Values')
+
+    # Display the bar chart using Streamlit's Plotly support
+    st.plotly_chart(fig)
+
+    # Display other details of the report
+    st.subheader("Expectation Type")
+    st.write(result['expectation_config']["expectation_type"])
+
+    st.subheader("Partial Unexpected List")
+    partial_unexpected_list = result['result']["partial_unexpected_list"]
+    if partial_unexpected_list:
+        for item in partial_unexpected_list:
+            st.write(item)
+    else:
+        st.write("No partial unexpected values found.")
+
 
 def main():
     # Set the app title
@@ -34,12 +81,12 @@ def main():
             if submit_button:
                 with st.spinner('Running your data quality checks'):
                     time.sleep(5)
-                    expectation_result = DQ_APP.run_checks(checks_input)
+                    expectation_result = DQ_APP.run_expectation(checks_input)
+                    #print(expectation_result.to_json_dict())
                     st.success('Your test has successfully been run! Get results')
-
                     with st.expander("Show Results"):
-                        st.subheader("Data Quality Checkpoint result")
-                        st.write(expectation_result)
+                        st.subheader("Data Quality result")
+                        display_test_result(expectation_result.to_json_dict())
 
             open_docs_button = st.button("Open Data Docs")
             if open_docs_button:
