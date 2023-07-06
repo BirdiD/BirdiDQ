2bef758e011aba3a4fb5b94ea3336840e3ba8304
BirdiD <diioula.doucoure@gmail.com>
2023-07-06 16:20:41 +0200

    add falcon model

---
+++ main_birdi/falcon_model.py
@@ -0,0 +1,46 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel, PeftConfig
+import torch
+
+
+def load_peft_model():
+    peft_model_id = "DioulaD/falcon-7b-instruct-qlora-ge-dq-v2"    
+    model = AutoModelForCausalLM.from_pretrained(
+            "tiiuae/falcon-7b-instruct",
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            trust_remote_code=True,
+        )
+    model = PeftModel.from_pretrained(model, peft_model_id)
+    model = model.merge_and_unload()
+
+    config = PeftConfig.from_pretrained(peft_model_id)
+
+    tknizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+    tknizer.pad_token = tknizer.eos_token
+    return model, tknizer
+
+
+
+def get_expectations(prompt, model, tknizer):
+  """
+  Convert natural language query to great expectation methods
+  Params:
+    prompt : Natural language query
+    model : Model download from huggingface hub
+    tknizer = Tokenizer from peft model
+  """
+  
+  encoding = tknizer(prompt, return_tensors="pt").to("cuda:0")
+  
+  with torch.inference_mode():
+    out = model.generate(
+        input_ids=encoding.input_ids,
+        attention_mask=encoding.attention_mask,
+        max_new_tokens=100, do_sample=True, temperature=0.3,
+        eos_token_id=tknizer.eos_token_id,
+        top_k=0
+    )
+
+  response = tknizer.decode(out[0], skip_special_tokens=True)
+  return response.split("\n")[1]
